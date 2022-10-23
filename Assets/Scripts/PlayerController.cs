9ccf82f5bb6fab8a745dc0cc86a70950a69a90f3
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2022-10-23 17:09:15 -0300

    ADD - New Obstacles and Move Vehicle Forward with RigidBody

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // I'll move the vehicle foward
+        // The "transform" is the attribute of the object, and the "Translate" is the "Translate" in Unity
+        // the "Time.deltaTime" guarantees that the code will be executed at the same time regardless of the PC processor
+        transform.Translate(Vector3.forward * Time.deltaTime * 10); // (0, 0, 1)
+    }
+}
